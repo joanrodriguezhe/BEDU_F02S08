04901cede98d320410ba018e69eaf8b688b0b266
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-03 19:04:40 -0700

    Ejemplo 02

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Ejemplo 1
-let title = document.querySelector('#title');
+let title = document.querySelector('#title1');
 
 document.getElementById('red').addEventListener('click', function () {
     title.style = 'color:red;';
@@ -10,3 +10,11 @@ document.getElementById('blue').addEventListener('click', function () {
 document.getElementById('black').addEventListener('click', function () {
     title.style = 'color:black;';
 });
+
+//Ejemplo2
+let title2 = document.querySelector('#title2');
+let input2 = document.getElementById('text');
+
+input2.addEventListener('input', function (event) {
+    title2.textContent = event.target.value;
+})
